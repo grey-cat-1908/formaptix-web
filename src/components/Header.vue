96e23c4b8cd73ca0ce3a93d3fe177726bca2e893
Vyacheslav <76677694+flyare1337@users.noreply.github.com>
2024-08-23 14:52:01 +0300

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,23 +9,23 @@ const authStore = useAuthStore()
   <header class="header">
     <div class="header-container">
       <div class="container">
-        <h1 class="">{{ getTitle() }}</h1>
+        <h1>{{ getTitle() }}</h1>
+        <div v-if="authStore.isAuthorized">
+          <button type="button">
+            {{ authStore.user.username }}
+          </button>
+          <button type="button" @click="authStore.logout">leave</button>
+        </div>
+        <div v-else>
+          <button type="button" @click="authStore.authDialogOpened = !authStore.authDialogOpened">
+            Войти
+          </button>
+        </div>
       </div>
     </div>
   </header>
 
-  <h1>{{ getTitle() }}</h1>
-  <div v-if="authStore.isAuthorized">
-    <button type="button">
-      {{ authStore.user.username }}
-    </button>
-    <button type="button" @click="authStore.logout">leave</button>
-  </div>
-  <div v-else>
-    <button type="button" @click="authStore.authDialogOpened = !authStore.authDialogOpened">
-      Войти
-    </button>
-  </div>
+
 </template>
 
 <style scoped></style>
