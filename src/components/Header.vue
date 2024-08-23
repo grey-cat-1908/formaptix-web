da2c497252cfadf127b3367755af3e234989e6ad
Vyacheslav <76677694+flyare1337@users.noreply.github.com>
2024-08-23 14:47:28 +0300

    fonts

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,13 @@ import { getTitle } from '@/utils/env'
 </script>
 
 <template>
-  <h1>{{ getTitle() }}</h1>
+  <header class="header">
+    <div class="header-container">
+      <div class="container">
+        <h1 class="">{{ getTitle() }}</h1>
+      </div>
+    </div>
+  </header>
 </template>
 
 <style scoped></style>
