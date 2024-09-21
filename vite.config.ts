5f1eca548c540455a3b40b8f3bd65f1af4c082e3
mrkrk <61203964+grey-cat-1908@users.noreply.github.com>
2024-09-21 20:50:37 +0300

    test cf fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: 'esbuild',
+    target: 'esnext'
+  },
   plugins: [vue()],
   resolve: {
     alias: {
